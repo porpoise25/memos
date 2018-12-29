b9265a14b49949533e249dd8b54cf0bbf7c8a41b
porpoise25 <46102455+porpoise25@users.noreply.github.com>
2018-12-29 20:26:34 +0900

    テキストの変更

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req,res) => {
     res.writeHead(200,{
         'Content-Type':'text/plain;charset=utf-8'
     });
-    res.write('HelloWorld');
+    res.write('test');
     res.end();
 })
 /**
