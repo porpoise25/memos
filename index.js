d73357a9fafb6f312872345060f2b8c7f573440a
porpoise25 <46102455+porpoise25@users.noreply.github.com>
2018-12-29 18:38:35 +0900

    初めてのコミット

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict;'
+const http = require('http');
+/**サーバーの作成
+ * 
+ */
+const server = http.createServer((req,res) => {
+    res.writeHead(200,{
+        'Content-Type':'text/plain;charset=utf-8'
+    });
+    res.write('HelloWorld');
+    res.end();
+})
+/**
+ *　サーバーを起動
+ */
+const port = 8000;
+server.listen(port,() => {
+    console.log('Listening on ' + port);
+})
