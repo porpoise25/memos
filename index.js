429e662b3273154896783370ea7c050d7289bcfb
porpoise25 <46102455+porpoise25@users.noreply.github.com>
2018-12-30 20:30:42 +0900

    入力フォームの作成、ejsの作成、外部CSSの読み込み

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 'use strict;'
 const http = require('http');
-/**サーバーの作成
- * 
+const ejs = require('ejs');
+var fs = require('fs');
+var qs = require('querystring');
+//ファイルの読み込み
+var template = fs.readFileSync(__dirname + '/index.ejs', 'utf-8');
+const style = fs.readFileSync('./css/common.css', 'utf-8');
+//グローバル変数
+var posts = [];
+var msg = '';
+
+/**
+ * ejsでメモリストを作る関数
+ */
+function renderList(posts, res) {
+    var data = ejs.render(template, {
+        posts: posts,
+        msg : msg,
+    });
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(data);
+    res.end();
+}
+
+/**
+ * サーバーの作成
  */
 const server = http.createServer((req,res) => {
-    res.writeHead(200,{
-        'Content-Type':'text/plain;charset=utf-8'
-    });
-    res.write('test');
-    res.end();
+    //cssの読み込み
+    switch(req.url){
+        case '/css/common.css':
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(style);   
+            break;
+    }
+    //保存ボタンが押されたとき
+    if (req.method === 'POST') {
+        req.data = '';
+        //フォームデータ受信
+        req.on('readable', function() {
+            req.data += req.read() || '';
+            console.log(req.data);
+        });
+        req.on('end', function() {
+            var query = qs.parse(req.data);
+            console.log(query);
+            //メモ表示:文字入力がないときは表示しない
+            if(query.memo){
+                msg = '';
+                posts.push(query.memo);
+            }else{
+                msg = '文字が入力されていません'
+            }
+            renderList(posts,res);
+        });
+    } else {
+        renderList(posts, res);
+    }
 })
 /**
  *　サーバーを起動
